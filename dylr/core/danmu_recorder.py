1efec878e422ce21e862cfb6ea5f79fd4f024da7
孤云 <28805930+Guyungy@users.noreply.github.com>
2024-02-25 15:03:00 +0800

    1

--- dylr/core/danmu_recorder.py
+++ dylr/core/danmu_recorder.py
@@ -1,19 +1,24 @@
-import _thread
-import gzip
-import os
-import time
-import traceback
+import _thread  # 用于启动心跳线程
+import gzip  # 用于解压缩 WebSocket 传输的数据
+import os  # 提供了与操作系统相关的功能，用于创建目录和文件
+import time  # 提供了与时间相关的功能，如时间戳转换等
+import traceback  # 用于打印异常的堆栈信息
 
-import websocket
-from google.protobuf import json_format
+import websocket  # 用于 WebSocket 连接
+from google.protobuf import json_format  # 用于处理 Google Protobuf 格式的数据
 
-from dylr.core import dy_api, app, record_manager
-from dylr.core.dy_protocol import PushFrame, Response, ChatMessage
-from dylr.util import logger, cookie_utils
-
+from dylr.core import dy_api, app, record_manager  # 引入其他模块
+from dylr.core.dy_protocol import PushFrame, Response, ChatMessage  # 引入自定义协议相关类
+from dylr.util import logger, cookie_utils  # 引入其他工具类
 
 class DanmuRecorder:
     def __init__(self, room, room_real_id, start_time=None):
+        """
+        初始化弹幕录制器
+        :param room: 房间信息对象
+        :param room_real_id: 房间真实 ID
+        :param start_time: 录制开始时间，默认为当前时间
+        """
         self.room = room
         self.room_id = room.room_id
         self.room_name = room.room_name
@@ -26,11 +31,15 @@ class DanmuRecorder:
         self.retry = 0
 
     def start(self):
+        """
+        开始录制弹幕
+        """
         if self.start_time is None:
             self.start_time = time.localtime()
         self.start_time_t = int(time.mktime(self.start_time))
         logger.info_and_print(f'开始录制 {self.room_name}({self.room_id}) 的弹幕')
 
+        # 创建保存文件的目录
         if not os.path.exists("download"):
             os.mkdir("download")
         if not os.path.exists("download/" + self.room_name):
@@ -52,12 +61,23 @@ class DanmuRecorder:
         self.ws.run_forever()
 
     def stop(self):
+        """
+        停止录制弹幕
+        """
         self.stop_signal = True
 
     def _onOpen(self, ws):
+        """
+        WebSocket 连接打开时的回调函数
+        启动心跳线程
+        """
         _thread.start_new_thread(self._heartbeat, (ws,))
 
     def _onMessage(self, ws: websocket.WebSocketApp, message: bytes):
+        """
+        接收到消息时的回调函数
+        解析并处理消息，将弹幕内容写入文件
+        """
         wssPackage = PushFrame()
         wssPackage.ParseFromString(message)
         logid = wssPackage.logid
@@ -89,9 +109,12 @@ class DanmuRecorder:
                 with open(self.filename, 'a', encoding='UTF-8') as file:
                     file.write(f"  <d p=\"{round(second, 2)},1,25,16777215,"
                                f"{int(now * 1000)},0,1602022773,0\" user=\"{user}\">{content}</d>\n")
-                # print(data['user']['nickName'] + ': ' + data['content'])
 
     def _heartbeat(self, ws: websocket.WebSocketApp):
+        """
+        心跳线程函数
+        发送心跳包，检测是否需要重新连接或停止录制
+        """
         t = 9
         while True:
             if app.stop_all_threads or self.stop_signal:
@@ -108,12 +131,6 @@ class DanmuRecorder:
                 if self.retry < 3 and self.danmu_amount == 0 and t > 30:
                     ws.close()
                     logger.warning_and_print(f'{self.room_name}({self.room_id}) 无法获取弹幕，正在重试({self.retry+1})')
-                    # time.sleep(5)
-                    # if dy_api.is_going_on_live(self.room):
-                    #     self.start_time = None  # 防止同名覆盖
-                    #     self.start()
-                    #     self.retry += 1
-                    #     break
                 now = time.time()
                 # 太长时间没弹幕，检测是否是下播了，可能下播后并没有断开 websocket
                 if t > 30 and now - self.last_danmu_time > 60:
@@ -123,11 +140,18 @@ class DanmuRecorder:
             time.sleep(1)
 
     def _onError(self, ws, error):
-        logger.error_and_print(f'[onError] {self.room_name}({self.room_id})弹幕录制抛出一个异常')
+        """
+        WebSocket 出错时的回调函数
+        打印异常信息
+        """
+        logger.error_and_print(f'[onError] {self.room_name}({self.room_id}) 弹幕录制抛出一个异常')
         logger.error_and_print(traceback.format_exc())
 
-
     def _onClose(self, ws, a, b):
+        """
+        WebSocket 连接关闭时的回调函数
+        完成录制文件的写入，并根据需要重试连接
+        """
         # 写入文件尾
         with open(self.filename, 'a', encoding='UTF-8') as file:
             file.write('</i>')
@@ -136,7 +160,7 @@ class DanmuRecorder:
             return
         if self.retry < 10 and dy_api.is_going_on_live(self.room):
             self.retry += 1
-            logger.info_and_print(f'{self.room_name}({self.room_id})弹幕录制重试({self.retry})')
+            logger.info_and_print(f'{self.room_name}({self.room_id}) 弹幕录制重试({self.retry})')
             self.start_time = None
             time.sleep(1)
             self.start()
