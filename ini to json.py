41a23ec4c1141ec2e6e9d8442dc9b16e8d0bfef9
孤云 <28805930+Guyungy@users.noreply.github.com>
2024-02-25 14:11:26 +0800

    1

--- ini to json.py
+++ ini to json.py
@@ -1,21 +1,27 @@
 import json
 
-# 定义一个空列表来存储房间信息
+# 初始化一个空列表存放房间信息
 rooms = []
 
-# 从URL_config.ini文件读取URL
+# 读取URL_config.ini文件中的URL
 with open('URL_config.ini', 'r', encoding='utf-8') as file:
     for line in file:
-        # 从每一行中解析出房间ID和主播名字
+        # 解析房间ID和主播名称
         parts = line.split(',')
         room_id = parts[0].split('/')[-1]
         name = parts[1].split(':')[1].strip()
-        
-        # 将解析出的信息添加到rooms列表
-        rooms.append({'id': room_id, 'name': name})
 
-# 将rooms列表保存到rooms.json文件
+        # 添加房间信息到列表，包括默认的其他字段
+        rooms.append({
+            "id": room_id,
+            "name": name,
+            "auto_record": True,
+            "record_danmu": True,
+            "important": False
+        })
+
+# 将列表信息保存到rooms.json文件
 with open('rooms_new.json', 'w', encoding='utf-8') as file:
     json.dump(rooms, file, ensure_ascii=False, indent=4)
 
-print("rooms.json文件已生成。")
+print("rooms_new.json文件已生成。")
